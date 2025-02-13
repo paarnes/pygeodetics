af40699437db7df9d639e13613cf9d2c9d906711
Per Helge Aarnes <114779344+paarnes@users.noreply.github.com>
2025-02-13 20:24:08 +0100

    add /geodetics

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,4 +14,5 @@ Notes:
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/geodetics')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/projections')))
